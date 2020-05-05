9bf53541b81461092bdf6f505e0294a514103139
赵建强 <13269982081@163.com>
2020-05-05 23:13:35 +0800

    go基础

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # go
+
+go在2007年9月形成构想,并于2009年11月发布,发明人是Robert Griesemer,Rob Pike和Ken Thompson. 
---
+++ ch1/helloworld.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("hello, 世界")
+}
+
+//go run 直接运行
+//go build 会编译输出一个可复用的二进制程序,可随时执行
